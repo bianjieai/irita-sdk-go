9d1ab1a6e20ac46d32c6ceb080f0924bb6182250
kaifei <kaifei@bianjie.ai>
2019-08-08 23:21:25 +0800

    define msgBankSend

--- types/types.go
+++ types/types.go
@@ -1,9 +1,15 @@
 package types
 
+import "github.com/irisnet/irishub/app/v1/bank"
+
 type NetworkType int
 
 const (
 	_ NetworkType = iota
 	Testnet
 	Mainnet
+)
+
+type (
+	MsgBankSend bank.MsgSend
 )
