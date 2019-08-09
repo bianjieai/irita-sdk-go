e8389181c6b2124edfa057d2961027237deac797
kaifei <kaifei@bianjie.ai>
2019-08-09 15:38:02 +0800

    remove unnecessary dependency

--- types/tx/cdc.go
+++ types/tx/cdc.go
@@ -1,12 +1,22 @@
 package tx
 
 import (
-	"github.com/irisnet/irishub/app/v1"
+	"github.com/irisnet/irishub/app/v1/auth"
+	"github.com/irisnet/irishub/app/v1/bank"
+	"github.com/irisnet/irishub/codec"
+	sdk "github.com/irisnet/irishub/types"
 	"github.com/tendermint/go-amino"
 )
 
 var Cdc *amino.Codec
 
 func init() {
-	Cdc = v1.MakeCodec()
+	//Cdc = v1.MakeCodec()
+	var cdc = codec.New()
+	bank.RegisterCodec(cdc)
+	auth.RegisterCodec(cdc)
+	sdk.RegisterCodec(cdc)
+	codec.RegisterCrypto(cdc)
+
+	Cdc = cdc
 }
