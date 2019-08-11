d27b9a01f7e3d18fa3920c190be8004912eb92c3
kaifei <kaifei@bianjie.ai>
2019-08-11 13:59:29 +0800

    rpc client add get block detail method

--- client/rpc/basic.go
+++ client/rpc/basic.go
@@ -3,6 +3,7 @@ package rpc
 import (
 	itypes "github.com/irisnet/sdk-go/client/types"
 	rpcclient "github.com/tendermint/tendermint/rpc/client"
+	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 	"github.com/tendermint/tendermint/types"
 )
 
@@ -10,6 +11,7 @@ type RPCClient interface {
 	BroadcastTx(broadcastType string, tx types.Tx) (itypes.BroadcastTxResult, error)
 	GetStatus() (ResultStatus, error)
 	GetTx(hash string) (ResultTx, error)
+	Block(height int64) (*ctypes.ResultBlock, error)
 }
 
 type client struct {
@@ -19,4 +21,8 @@ type client struct {
 func NewClient(nodeUrl string) RPCClient {
 	rpc := rpcclient.NewHTTP(nodeUrl, "/websocket")
 	return &client{rpc: rpc}
+}
+
+func (c *client) Block(height int64) (*ctypes.ResultBlock, error) {
+	return c.rpc.Block(&height)
 }
