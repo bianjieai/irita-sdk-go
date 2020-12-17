2df79589740cfb22bd5e862f3c08af163a5bd068
seekelvis <seekelvis@163.com>
2020-12-17 17:13:24 +0800

    The Oracle module has been added, but it has not passed the test.

---
+++ modules/oracle/codec.go
@@ -0,0 +1,28 @@
+package oracle
+
+import (
+	"github.com/bianjieai/irita-sdk-go/codec"
+	"github.com/bianjieai/irita-sdk-go/codec/types"
+	cryptocodec "github.com/bianjieai/irita-sdk-go/crypto/codec"
+	sdk "github.com/bianjieai/irita-sdk-go/types"
+)
+
+var (
+	amino     = codec.NewLegacyAmino()
+	ModuleCdc = codec.NewAminoCodec(amino)
+)
+
+func init() {
+	cryptocodec.RegisterCrypto(amino)
+	amino.Seal()
+}
+
+func RegisterInterfaces(registry types.InterfaceRegistry) {
+	registry.RegisterImplementations(
+		(*sdk.Msg)(nil),
+		&MsgCreateFeed{},
+		&MsgStartFeed{},
+		&MsgPauseFeed{},
+		&MsgEditFeed{},
+	)
+}
