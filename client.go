e92662dce71f59d2386c2b3240265a49469c4a26
junjie.chen <xiyueedu@gmail.com>
2021-04-25 13:58:48 +0800

    rollback method AppCodec

--- client.go
+++ client.go
@@ -26,6 +26,21 @@ import (
 	txtypes "github.com/bianjieai/irita-sdk-go/types/tx"
 )
 
+var registers = []codec.RegisterInterfaces{
+	perm.RegisterInterfaces,
+	bank.RegisterInterfaces,
+	identity.RegisterInterfaces,
+	token.RegisterInterfaces,
+	token.RegisterInterfaces,
+	record.RegisterInterfaces,
+	nft.RegisterInterfaces,
+	nft.RegisterInterfaces,
+	service.RegisterInterfaces,
+	node.RegisterInterfaces,
+	params.RegisterInterfaces,
+	wasm.RegisterInterfaces,
+}
+
 // IRITAClient define a group of api to access c network
 type IRITAClient struct {
 	logger         log.Logger
@@ -45,6 +60,16 @@ type IRITAClient struct {
 	Params   params.Client
 	WASM     wasm.Client
 	Oracle   oracle.Client
+}
+
+// AppCodec return a Marshaler of the protobuf
+func AppCodec(rs ...codec.RegisterInterfaces) codec.Marshaler {
+	encodingConfig := makeEncodingConfig()
+	registers = append(registers, rs...)
+	for _, register := range registers {
+		register(encodingConfig.InterfaceRegistry)
+	}
+	return encodingConfig.Marshaler
 }
 
 // NewIRITAClient return a instance of the  IRITAClient
