34273089837540321f30913d9e3dee1ac4bd6237
sheldon <997166273@qq.com>
2021-07-02 09:59:24 +0800

    add keepalive

--- modules/grpc_client.go
+++ modules/grpc_client.go
@@ -4,7 +4,9 @@ import (
 	"github.com/bianjieai/irita-sdk-go/types"
 	"github.com/prometheus/common/log"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 	"sync"
+	"time"
 )
 
 var clientConnSingleton *grpc.ClientConn
@@ -15,8 +17,15 @@ type grpcClient struct {
 
 func NewGRPCClient(url string) types.GRPCClient {
 	once.Do(func() {
+		var kacp = keepalive.ClientParameters{
+			Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+			Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+			PermitWithoutStream: true,             // send pings even without active streams
+		}
+
 		dialOpts := []grpc.DialOption{
 			grpc.WithInsecure(),
+			grpc.WithKeepaliveParams(kacp),
 		}
 		clientConn, err := grpc.Dial(url, dialOpts...)
 		if err != nil {
