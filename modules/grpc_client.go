29af99e9818902f67f90a0d32e0cfd945eb8120e
sheldon <997166273@qq.com>
2021-07-07 14:17:25 +0800

    modify grpc_client: delete the grpc client: keepalive opts; delete the wasm module's code: defer conn.close()

--- modules/grpc_client.go
+++ modules/grpc_client.go
@@ -2,12 +2,10 @@ package modules
 
 import (
 	"sync"
-	"time"
 
 	"github.com/bianjieai/irita-sdk-go/types"
 	"github.com/prometheus/common/log"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/keepalive"
 )
 
 var clientConnSingleton *grpc.ClientConn
@@ -18,15 +16,9 @@ type grpcClient struct {
 
 func NewGRPCClient(url string) types.GRPCClient {
 	once.Do(func() {
-		var kacp = keepalive.ClientParameters{
-			Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
-			Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
-			PermitWithoutStream: true,             // send pings even without active streams
-		}
 
 		dialOpts := []grpc.DialOption{
 			grpc.WithInsecure(),
-			grpc.WithKeepaliveParams(kacp),
 		}
 		clientConn, err := grpc.Dial(url, dialOpts...)
 		if err != nil {
