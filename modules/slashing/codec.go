415d937ea8bcfdfef6415f42ee817a4d7af90b08
prolenking <37539652+prolenking@users.noreply.github.com>
2021-09-30 14:07:03 +0800

    add slashing msg registration (#76)
    Co-authored-by: lizhi <zhi.li@whats-egg.com>

---
+++ modules/slashing/codec.go
@@ -0,0 +1,32 @@
+package slashing
+
+import (
+	"github.com/bianjieai/irita-sdk-go/codec"
+	"github.com/bianjieai/irita-sdk-go/codec/types"
+	cryptocodec "github.com/bianjieai/irita-sdk-go/crypto/codec"
+	sdk "github.com/bianjieai/irita-sdk-go/types"
+)
+
+const (
+	// ModuleName define the module name
+	ModuleName = "slashing"
+)
+
+var (
+	amino = codec.NewLegacyAmino()
+	// ModuleCdc define the codec for wasm module
+	ModuleCdc = codec.NewAminoCodec(amino)
+)
+
+func init() {
+	cryptocodec.RegisterCrypto(amino)
+	amino.Seal()
+}
+
+// RegisterInterfaces regisger the implement of the msg interface for InterfaceRegistry
+func RegisterInterfaces(registry types.InterfaceRegistry) {
+	registry.RegisterImplementations(
+		(*sdk.Msg)(nil),
+		&MsgUnjailValidator{},
+	)
+}
